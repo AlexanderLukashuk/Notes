4d6ce1aef0922b920a655c2adce4cd00d3485033
AlexanderLukashuk <upiter465@gmail.com>
2024-03-12 17:04:17 +0500

    created interface for database context

---
+++ Core/Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+
+namespace Notes.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
