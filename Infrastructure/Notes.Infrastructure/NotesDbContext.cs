dbca2618ff3474557d2c018d53335ce7456dfa5c
AlexanderLukashuk <upiter465@gmail.com>
2024-03-12 17:02:00 +0500

    created database context

---
+++ Infrastructure/Notes.Infrastructure/NotesDbContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Infrastructure.EntityTypeConfiguration;
+
+namespace Notes.Infrastructure
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
