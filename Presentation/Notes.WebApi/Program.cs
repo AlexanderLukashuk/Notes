3964158e433ae51afa47a3be7083f01486c71858
AlexanderLukashuk <upiter465@gmail.com>
2024-03-23 17:21:10 +0500

    added services and configurations

--- Presentation/Notes.WebApi/Program.cs
+++ Presentation/Notes.WebApi/Program.cs
@@ -1,6 +1,49 @@
+using System.Reflection;
+using Notes.Application;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Interfaces;
+using Notes.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddPersistence();
+builder.AddApplication();
+
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<NotesDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception exception)
+    {
+
+    }
+}
+
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.MapControllers();
 
 app.Run();
