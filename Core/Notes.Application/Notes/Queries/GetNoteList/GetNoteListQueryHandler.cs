45d8bd4e8693407822426ac657638f947f2939c5
AlexanderLukashuk <upiter465@gmail.com>
2024-03-23 16:42:16 +0500

    added dependency injection for db context and mediatr

--- Core/Notes.Application/Notes/Queries/GetNoteList/GetNoteListQueryHandler.cs
+++ Core/Notes.Application/Notes/Queries/GetNoteList/GetNoteListQueryHandler.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Notes.Application.Interfaces;
 
 namespace Notes.Application.Notes.Queries.GetNoteList
@@ -16,9 +14,14 @@ namespace Notes.Application.Notes.Queries.GetNoteList
         public GetNoteListQueryHandler(INotesDbContext dbContext, IMapper mapper) =>
             (_dbContext, _mapper) = (dbContext, mapper);
 
-        public Task<NoteListVm> Handle(GetNoteListQuery request, CancellationToken cancellationToken)
+        public async Task<NoteListVm> Handle(GetNoteListQuery request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var notesQuery = await _dbContext.Notes
+                .Where(note => note.UserId == request.UserId)
+                .ProjectTo<NoteLookupDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            return new NoteListVm { Notes = notesQuery };
         }
     }
 }
