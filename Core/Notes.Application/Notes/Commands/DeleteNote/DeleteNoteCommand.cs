67fb9321387c906c7d8bd1a0154bb27ae1f61932
AlexanderLukashuk <upiter465@gmail.com>
2024-03-23 15:40:57 +0500

    created command to delete note

---
+++ Core/Notes.Application/Notes/Commands/DeleteNote/DeleteNoteCommand.cs
@@ -0,0 +1,10 @@
+using MediatR;
+
+namespace Notes.Application.Notes.Commands.DeleteNote
+{
+    public class DeleteNoteCommand : IRequest
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+    }
+}
