08cd69088bee09e828c0160dbb891bc2389188d0
AlexanderLukashuk <upiter465@gmail.com>
2024-04-11 03:20:28 +0500

    created swagger documentation

--- Presentation/Notes.WebApi/Models/CreateNoteDto.cs
+++ Presentation/Notes.WebApi/Models/CreateNoteDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace Notes.WebApi.Models
 {
     public class CreateNoteDto : IMapWith<CreateNoteCommand>
     {
+        [Required]
         public string Title { get; set; }
         public string Details { get; set; }
 
